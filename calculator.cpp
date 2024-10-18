8105007a8c55e3dfda81984a42eba42197130574
Mohith Sai Gorla <46181469+iammohith@users.noreply.github.com>
2024-10-18 23:10:09 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,40 +5,58 @@ using namespace std;
 
 int main()
 {
-	float num1, num2;
-	char op;
-	
-	cout << "Enter operator either + or - or * or /: ";
-	cin >> op;
-	
-	cout << "Enter the first number:" ;
-	cin >> num1;
-	
-	cout << "Enter the second number:" ;
-	cin >> num2;
-	
-	switch(op)
-	{
-		case '+':
-			cout << num1 << "+" << num2 << "=" << num1 + num2 << endl;
-			break;
-		
-		case '-':
-			cout << num1 << "-" << num2 << "=" << num1 - num2 << endl;
-			break;
-		
-		case '*':
-			cout << num1 << "*" << num2 << "=" << num1 * num2 << endl;
-			break;
-		
-		case '/':
-			cout << num1 << "/" << num2 << "=" << num1 / num2 << endl;
-			break;
-		
-		default:
-			cout << "Error! operator is not correct" << endl;
-			break;	
-	}
-	
-	return 0;
+    // Declare variables for two numbers and the operator
+    float num1, num2; 
+    char op;  // Operator: +, -, *, /
+
+    // Prompt the user to input an operator
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    // Prompt the user to input the first number
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    // Prompt the user to input the second number
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Switch case to perform the appropriate arithmetic operation based on the input operator
+    switch (op)
+    {
+        // Addition
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        
+        // Subtraction
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+
+        // Multiplication
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+
+        // Division
+        case '/':
+            // Check for division by zero before performing the operation
+            if (num2 != 0)
+            {
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            }
+            else
+            {
+                cout << "Error! Division by zero is not allowed." << endl;
+            }
+            break;
+
+        // Handle incorrect operator input
+        default:
+            cout << "Error! Operator is not correct." << endl;
+            break;    
+    }
+
+    return 0;
 }
